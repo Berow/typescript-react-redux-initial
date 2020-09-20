a50c138f99f5600969a5d03185a520aed3344d59
Berow <52027524+Berow@users.noreply.github.com>
2020-09-20 15:37:10 +0300

    ts

--- src/app/bus/profile/actions.ts
+++ src/app/bus/profile/actions.ts
@@ -1,20 +1,20 @@
 // Types
-import { types } from './types';
+import { types, ProfileActionTypes, Profile } from './types';
 
 export const profileActions = {
     // Sync
-    fillProfile: (profile) => {
+    fillProfile: (profile: Profile): ProfileActionTypes => {
         return {
             type: types.FILL_PROFILE,
             payload: profile
         };
     },
-    startFetching: () => {
+    startFetching: (): ProfileActionTypes => {
         return {
             type: types.START_FETCHING,
         };
     },
-    stopFetching: () => {
+    stopFetching: (): ProfileActionTypes => {
         return {
             type: types.STOP_FETCHING,
         };
--- src/app/bus/profile/types.ts
+++ src/app/bus/profile/types.ts
@@ -4,3 +4,25 @@ export const types = {
     START_FETCHING: 'START_FETCHING',
     STOP_FETCHING: 'STOP_FETCHING',
 };
+
+export type Profile = {
+    name: string;
+}
+
+type StartProfileFetchingAction = {
+    type: typeof types.START_FETCHING;
+};
+
+type StopProfileFetchingAction = {
+    type: typeof types.STOP_FETCHING;
+};
+
+type FillProfileAction = {
+    type: typeof types.FILL_PROFILE,
+    payload: Starships;
+};
+
+export type ProfileActionTypes =
+    | StartProfileFetchingAction
+    | StopProfileFetchingAction
+    | FillProfileAction;
