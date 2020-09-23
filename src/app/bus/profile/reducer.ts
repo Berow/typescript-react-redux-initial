c98b7d9f2fb8ae6aca43b90b5e3251f9c8045871
Berow <52027524+Berow@users.noreply.github.com>
2020-09-23 15:46:45 +0300

    ts

--- src/app/bus/profile/reducer.ts
+++ src/app/bus/profile/reducer.ts
@@ -1,37 +1,33 @@
 // Types
-import {
-    FILL_PROFILE,
-    START_FETCHING,
-    STOP_FETCHING,
-    ProfileActionTypes, Profile
-} from './types';
+import { FILL_PROFILE, START_FETCHING, STOP_FETCHING, ProfileActionTypes, Profile } from './types';
 
 export type ProfileState = {
-    profile: Profile,
-    isFetching: Boolean
-}
+  profile: Profile;
+  isFetching: boolean;
+};
 
 const initialState: ProfileState = {
-    profile: {
-        firstName: 'Уолтер',
-        lastName: 'Уайт',
-    },
-    isFetching: false,
+  profile: {
+    firstName: 'Уолтер',
+    lastName: 'Уайт',
+  },
+  isFetching: false,
 };
 
 export const profileReducer = (state = initialState, action: ProfileActionTypes): ProfileState => {
-    switch (action.type) {
-        case FILL_PROFILE:
-            return { ...state, ...action.payload };
+  switch (action.type) {
+    case FILL_PROFILE:
+      return { ...state, ...action.payload };
 
-        case START_FETCHING:
-            return { ...state, isFetching: true };
+    case START_FETCHING:
+      return { ...state, isFetching: true };
 
-        case STOP_FETCHING:
-            return { ...state, isFetching: false };
+    case STOP_FETCHING:
+      return { ...state, isFetching: false };
 
-        default:
-            const x: never = action;
-    }
-    return state;
+    default:
+      // eslint-disable-next-line no-case-declarations
+      const x: never = action;
+  }
+  return state;
 };
--- src/app/bus/profile/types.ts
+++ src/app/bus/profile/types.ts
@@ -1,29 +1,27 @@
-
 // Sync
 export const FILL_PROFILE = 'FILL_PROFILE';
 export const START_FETCHING = 'START_FETCHING';
 export const STOP_FETCHING = 'STOP_FETCHING';
 
-
 export type Profile = {
-    firstName: string,
-    lastName: string,
-}
+  firstName: string;
+  lastName: string;
+};
 
 type StartProfileFetchingAction = {
-    type: typeof START_FETCHING;
+  type: typeof START_FETCHING;
 };
 
 type StopProfileFetchingAction = {
-    type: typeof STOP_FETCHING;
+  type: typeof STOP_FETCHING;
 };
 
 type FillProfileAction = {
-    type: typeof FILL_PROFILE,
-    payload: Profile;
+  type: typeof FILL_PROFILE;
+  payload: Profile;
 };
 
 export type ProfileActionTypes =
-    | StartProfileFetchingAction
-    | StopProfileFetchingAction
-    | FillProfileAction;
+  | StartProfileFetchingAction
+  | StopProfileFetchingAction
+  | FillProfileAction;
